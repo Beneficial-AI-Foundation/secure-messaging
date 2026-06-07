2921ff38d5a338e6f2ab771eaedf7ddfa96a6bcc
sbursuc <48085107+sbursuc@users.noreply.github.com>
2026-06-07 19:25:35 +0200

    Remove displayed Lean code pills from docs

--- docs/SecureMessagingDocs/Chapters/CKA/FromDDH.lean
+++ docs/SecureMessagingDocs/Chapters/CKA/FromDDH.lean
@@ -34,22 +34,6 @@ CKA from DDH.
 :::definition "cka_from_ddh" (parent := "cka_cka_from_ddh") (lean := "ddhCKA")
 $`\todo`
 
-```anchor ddhCKA (project := ".") (module := SecureMessaging.CKA.FromDDH.Construction)
-def ddhCKA (F G : Type) [Field F] [Fintype F] [DecidableEq F] [SampleableType F]
-    [AddCommGroup G] [Module F G] [SampleableType G]
-    (gen : G) : CKAScheme ProbComp (G × F) (F ⊕ G) G G F where
-  initKeyGen := do
-    let x ← $ᵗ F
-    return (x • gen, x)
-  initA := fun (h, _) => return .inr h
-  initB := fun (_, x) => return .inl x
-  sendA := ddhCKA.send gen
-  sendA_rleak := ddhCKA.send_rleak gen
-  sendB := ddhCKA.send gen
-  sendB_rleak := ddhCKA.send_rleak gen
-  recvA := ddhCKA.recv
-  recvB := ddhCKA.recv
-```
 
 {usesLabel}`uses` {uses "cka"}[] · {githubLabel}`github` {githubIssue 10}[]
 :::
@@ -62,10 +46,6 @@ $`\todo`
 
 $$`\Pr[\,\textsf{correctnessExp} = \mathsf{true}\,] = 1`
 
-```anchor correctness (project := ".") (module := SecureMessaging.CKA.FromDDH.Correctness)
-theorem correctness [DecidableEq G] (adv : CKACorrectnessAdversary G G) :
-    Pr[= true | correctnessExp (ddhCKA F G gen) adv] = 1
-```
 
 {usesLabel}`uses` {uses "cka_from_ddh"}[] · {uses "cka_correct"}[]
 :::
